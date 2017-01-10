ca7557f76ce5741195cdb8f2d1358421afe1e249
tangjuncheng <tangjuncheng@38a5f1e3-14a2-4cf0-a27c-a24aa698f813>
2017-01-10 08:12:00 +0000

    git-svn-id: http://192.168.1.10/svn/repos/hymap/trunk/hyMap@13179 38a5f1e3-14a2-4cf0-a27c-a24aa698f813

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+* @Author: FunctionRun
+* @Date:   2017-01-10 10:15:25
+* @Last Modified by:   hydata
+* @Last Modified time: 2017-01-10 16:05:17
+* @Email: zhangyujie3344521@163.com
+* @File Path: /Users/zhangyujie/GitHub/FEscaffold/index.js
+* @File Name: index.js
+* @Descript:
+*/
+
+'use strict';
+	console.log('项目测试成功');
+
+
+setTimeout(()=> {
+
+    console.log(author);
+
+}, 4000);
