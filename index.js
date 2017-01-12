927e3de2186a2bf67f78c1b1644cd48cb743d4b7
wuxiaoqiang <wuxiaoqiang@38a5f1e3-14a2-4cf0-a27c-a24aa698f813>
2017-01-12 01:58:16 +0000

    git-svn-id: http://192.168.1.10/svn/repos/hymap/trunk/hyMap@13259 38a5f1e3-14a2-4cf0-a27c-a24aa698f813

--- index.js
+++ index.js
@@ -15,36 +15,51 @@ import hyMap from './src/hymap';
 
 var map = new hyMap();
 map.init(document.getElementById('map'));
+let options = {
+    show: true, //地图的显示状态 true为显示 false 为不显示
+    map: 'shandong', //当前地图显示哪个地图
+    roam: 'true', //地图是否开启缩放、平移功能
+    center: [118.62778784888256, 36.58892145091036], //当前视角中心: [经度, 纬度]
+    zoom: 3, //当前地图缩放比例
+    scaleLimit: [5, 12], //滚轮缩放的边界
+    itemStyle: '', //地图上每块区域的样式
+    selectedMode: '', //地图区域的选中模式
+    theme: '', //地图风格
+    regions: '', //  name: '',特殊区域的样式
+    label: '', //文本标签样式
+    series: []
+};
+fetch('test/station.json').then(response => response.json()).then(function(data) {
 
-fetch('test/station.json').then(function(response) {
+    options.series.push({
+        data: data, //{x,y,value}
+        type: 'point', // point|line|polygon|chart|..
+        symbol: 'circle', //circle|react|icon
+        symbolSize: '', //[min,max]
+        symbolStyle: '', //{normal,emphasis}
+        label: 'mc'
 
-    return response.json();
+    });
 
-}).then(function(data) {
 
-    var options = {
-        show: true, //地图的显示状态 true为显示 false 为不显示
-        map: 'shandong', //当前地图显示哪个地图
-        roam: 'true', //地图是否开启缩放、平移功能
-        center: [118.62778784888256, 36.58892145091036], //当前视角中心: [经度, 纬度]
-        zoom: 3, //当前地图缩放比例
-        scaleLimit: [5, 7], //滚轮缩放的边界
-        itemStyle: '', //地图上每块区域的样式
-        selectedMode: '', //地图区域的选中模式
-        theme: '', //地图风格
-        regions: '', //  name: '',特殊区域的样式
-        label: '', //文本标签样式
-        series: [{
+
+}).then(
+    fetch('test/car_2012.json').then(response => response.json()).then(function(data) {
+
+        options.series.push({
             data: data,
             type: 'point',
-            symbol: '',
+            symbol: 'icon:img/jingli.png',
             symbolSize: '',
-            symbolStyle: '',
+            symbolStyle: {
+                'normal': {
+                    width: 15,
+                    height: 15
+                }
+            },
             label: 'mc'
+        });
 
-        }]
-    };
 
-    map.setOption(options);
-
-});
+    })
+).then(() => map.setOption(options));
