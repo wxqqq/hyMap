c28b020f84b7660cc47cb2bbb5c33fa552204c42
tangjuncheng <tangjuncheng@38a5f1e3-14a2-4cf0-a27c-a24aa698f813>
2017-01-18 02:29:20 +0000

    git-svn-id: http://192.168.1.10/svn/repos/hymap/trunk/hyMap@13454 38a5f1e3-14a2-4cf0-a27c-a24aa698f813

--- test/container/App/App.js
+++ test/container/App/App.js
@@ -1,8 +1,8 @@
 /*
  * @Author: zhangyujie
  * @Date:   2016-05-09 11:33:52
- * @Last Modified by:   FunctionRun
- * @Last Modified time: 2016-06-30 11:34:18
+ * @Last Modified by:   hydata
+ * @Last Modified time: 2017-01-17 19:08:57
  * @Email: zhangyujie3344521@163.com
  * @File Path: /Users/zhangyujie/node/www/tueasy5.0/visual/Test/container/App/App.js
  * @File Name: App.js
@@ -11,18 +11,19 @@
 
 'use strict';
 
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import Nav from '../../components/Nav/Nav';
 
 class App extends Component {
 
     render() {
 
-        return (
-            <div className = 'App' >
-              <Nav />
-              {this.props.children}
-            </div>
+        return ( < div className = 'App' >
+            < Nav / > {
+                this.props.children
+            } < /div>
         );
     }
 }
