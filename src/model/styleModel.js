81f8c2d88acc0141f46a65271edc37b28ab565e6
wuxiaoqiang <wuxiaoqiang@38a5f1e3-14a2-4cf0-a27c-a24aa698f813>
2017-02-13 01:49:11 +0000

    git-svn-id: http://192.168.1.10/svn/repos/hymap/trunk/hyMap@13973 38a5f1e3-14a2-4cf0-a27c-a24aa698f813

---
+++ src/model/styleModel.js
@@ -0,0 +1,31 @@
+'use strick';
+
+export default {
+    'normal': {
+        symbol: 'circle', //circle|react|icon:img/jingli.png
+        radius: 3,
+        symbolSize: '', //图形大小
+        strokeWidth: 3, //边框宽度
+        strokeColor: 'red', //边框颜色
+        fillColor: 'green',
+        opacity: 0.2 //透明度
+    },
+    'hover': {
+        symbol: 'circle', //circle|react|icon:img/jingli.png
+        radius: 3,
+        symbolSize: '', //图形大小
+        strokeWidth: 3, //边框宽度
+        strokeColor: 'red', //边框颜色
+        fillColor: 'green',
+        opacity: 0.2 //透明度
+    },
+    'select': {
+        symbol: 'circle', //circle|react|icon:img/jingli.png
+        radius: 3,
+        symbolSize: '', //图形大小
+        strokeWidth: 3, //边框宽度
+        strokeColor: 'red', //边框颜色
+        fillColor: 'green',
+        opacity: 0.2 //透明度
+    }
+};
