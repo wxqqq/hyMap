f6959902c3066f72b663ae1ace6893de01a625cf
wuxiaoqiang <wuxiaoqiang@38a5f1e3-14a2-4cf0-a27c-a24aa698f813>
2017-02-22 11:13:27 +0000

    git-svn-id: http://192.168.1.10/svn/repos/hymap/trunk/hyMap@14161 38a5f1e3-14a2-4cf0-a27c-a24aa698f813

---
+++ routes.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: zhangyujie
+ * @Date:   2016-05-09 11:31:09
+ * @Last modified by:   deyuhua
+ * @Last modified time: 2016-06-30T14:58:24+08:00
+ * @Email: zhangyujie3344521@163.com
+ * @File Path: /Users/zhangyujie/node/www/tueasy5.0/Visual/routes.js
+ * @File Name: routes.js
+ * @Descript:
+ */
+
+'use strict';
+import React from 'react';
+import {
+    Route,
+    IndexRoute
+} from 'react-router';
+import {
+    App,
+    circle,
+    rect,
+    icon,
+    line,
+    chart,
+    mapmethod
+
+} from './test/container/index';
+
+
+const route = (
+    <Route path="/" component={App}>
+     <IndexRoute component={circle}/>
+        <Route path = 'chart' component = {chart} />
+        <Route path = 'circle' component = {circle} />
+        <Route path = 'icon' component = {icon} />
+        <Route path = 'line' component = {line} />
+        <Route path = 'rect' component = {rect} />
+        <Route path = 'mapmethod' component = {mapmethod} />
+    </Route>
+)
+
+export default route
