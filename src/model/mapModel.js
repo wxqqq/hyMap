ce263c426fb452029cc8c0f5ae782f8e11f8be97
wuxiaoqiang <304861063@qq.com>
2017-10-18 14:56:42 +0800

    增加routeName参数

--- src/model/mapModel.js
+++ src/model/mapModel.js
@@ -15,6 +15,7 @@ export default {
     itemStyle: {}, //地图上每块区域的样式
     selectedMode: '', //地图区域的选中模式
     theme: 'dark', //地图风格
+    routeName:'road_jining',
     regions: [], //  name: '',特殊区域的样式
     label: '', //文本标签样式
     series: [],
